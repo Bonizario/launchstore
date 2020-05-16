8995bfbbe8007f2fd533aa273f0b4a619ac9567b
Bonizario <gabrielbonizario98@gmail.com>
2020-05-16 19:55:31 -0300

    Generating data with the faker library for testing

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -58,7 +58,7 @@ const Base = {
 
       Object.keys(fields).map(key => {
         keys.push(key);
-        values.push(fields[key]);
+        values.push(`'${fields[key]}'`);
       });
 
       const query = `
