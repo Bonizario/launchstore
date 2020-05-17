521500289d920da8c0152abaa3fc0dedc9295140
Bonizario <gabrielbonizario98@gmail.com>
2020-05-16 23:23:17 -0300

    Adjusting the search bar

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -13,32 +13,26 @@ module.exports = {
 
     return results.rows;
   },
-  async search(params) {
-    const { filter, category } = params;
-
-    let query = ``,
-      filterQuery = `WHERE`;
+  async search({ filter, category }) {
+    let query = `
+      SELECT products.*, categories.name AS category_name
+      FROM products
+      LEFT JOIN categories ON (categories.id = products.category_id)
+      WHERE 1 = 1
+    `;
 
     if (category) {
-      filterQuery = `${filterQuery}
-        products.category_id = ${category}
-        AND
+      query += ` AND products.category_id = ${category}`;
+    }
+
+    if (filter) {
+      query += `
+        AND (products.name ILIKE '%${filter}%'
+        OR products.description ILIKE '%${filter}%')
       `;
     }
 
-    filterQuery = `
-      ${filterQuery}
-      products.name ILIKE '%${filter}%'
-      OR products.description ILIKE '%${filter}%'
-    `;
-
-    query = `
-      SELECT products.*,
-        categories.name AS category_name
-      FROM products
-      LEFT JOIN categories ON (categories.id = products.category_id)
-      ${filterQuery}
-    `;
+    query += ` AND status != 0`;
 
     const results = await db.query(query);
     return results.rows;
